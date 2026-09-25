5ca866301fd95b3f4dc13d2ed9701dd6ec8c66bd
SSittt <sitorasolieva03@gmail.com>
2026-09-25 10:00:00 +0700

    #1  Создали класс: BankAccount и Transaction. В main  протестировали

---
+++ 1109/1109/Class2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1109
+{
+    // recoed - состояние объектов этого класса нельзя изменить 
+    internal record Transaction (decimal Amount, DateTime Date, string Note);
+    
+    // более обширенный - можно его использовать когда нужно произвести проверку 
+    //internal record Transaction
+    //{
+    //    public decimal Amount { get;  }
+    //    public DateTime Date { get; }
+    //    public string Note { get; }
+
+    //    public Transaction(decimal Amount, DateTime Date, string
+    //        Note)
+    //    {
+    //        this.Amount = Amount;
+    //        this.Date = Date;
+    //        this.Note = Note;
+    //    }
+    //}
+}
