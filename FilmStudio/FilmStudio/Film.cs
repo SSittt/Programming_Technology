d9deae903c4bec0a2e7ae37422f2df411adc33b4
SSittt <sitorasolieva03@gmail.com>
2026-09-26 09:52:36 +0700

    #3 Создание классов: Director, Film and Studio. In main

---
+++ FilmStudio/FilmStudio/Film.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStudio
+{
+    /// <summary>
+    /// Фильмы 
+    /// </summary>
+    internal class Film
+    {
+        /// <summary>
+        /// свойство класса 
+        /// </summary>
+        public int idFilm { get; set; }
+        public string Title { get; set; }
+        public int StudioId { get; set; }
+        public int DirectorId { get; set; }
+        public int Year { get; set; }
+
+        public decimal Budget { get; set; }
+
+        /// <summary>
+        /// конструкторы
+        /// </summary>
+        public Film()
+        {
+
+        }
+        public Film(int idFilm, string title, int studioId,
+                int directorId, int year, decimal budget)
+        {
+            if (idFilm <= 0)
+                throw new ArgumentOutOfRangeException("ID фильма должен быть положительным");
+
+            if (string.IsNullOrWhiteSpace(title))
+                throw new ArgumentOutOfRangeException("Название фильма не может быть пустым");
+
+            if (studioId <= 0)
+                throw new ArgumentOutOfRangeException("ID студии должен быть положительным");
+
+            if (directorId <= 0)
+                throw new ArgumentOutOfRangeException("ID режиссёра должен быть положительным");
+
+            if (year <= 0)
+                throw new ArgumentOutOfRangeException("Год фильма указан некорректно");
+
+            if (budget < 0)
+                throw new ArgumentOutOfRangeException("Бюджет не может быть отрицательным");
+
+            this.idFilm = idFilm;
+            this.Title = title;
+            this.StudioId = studioId;
+            this.DirectorId = directorId;
+            this.Year = year;
+            this.Budget = budget;
+        }
+
+        /// <summary>
+        /// явл ли фильм дорогим 
+        /// </summary>
+        public bool IsExpensive
+        {
+            get
+            {
+                return Budget > 100000000;
+            }
+        }
+        /// <summary>
+        /// инфа о фильие 
+        /// </summary>
+        public string GetInfo()
+        {
+            return ($"{Title} ({Year}, {Budget} руб.)");
+        }
+    }
+}
---
+++ FilmStudio/FilmStudio/Studio.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmStudio
+{
+    /// <summary>
+    /// Студии
+    /// </summary>
+    internal class Studio
+
+    {
+        /// <summary>
+        /// свойство класса 
+        /// </summary>
+        public int idStudio { get; set; }
+        public string name { get; set; }
+        public string country { get; set; }
+
+        /// <summary>
+        /// конструкторы 
+        /// </summary>
+        public Studio()
+        {
+
+        }
+
+
+        public Studio(int idStudio, string name, string country)
+        {
+            if (idStudio <= 0)
+                throw new ArgumentException("ID студии должен быть положительным");
+
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Название студии не может быть пустым");
+
+            if (string.IsNullOrWhiteSpace(country))
+                throw new ArgumentException("Страна не может быть пустой");
+
+            this.idStudio = idStudio;
+            this.name = name;
+            this.country = country;
+        }
+
+        /// <summary>
+        /// вычислем иностранные студии - свойство так как характеристика объекта 
+        /// </summary>
+        public bool IsForeign
+        {
+            get
+            {
+                return country != "Россия";
+            }
+        }
+
+        /// <summary>
+        /// метод для формата вывода инфу о студии
+        /// </summary>
+        public string GetInfo()
+        {
+            return $"{name} ({country})";
+        }
+
+
+    }
+
+
+}
